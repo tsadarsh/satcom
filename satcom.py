95e90577696c632cb65d714848692d84bcf93141
Adarsh <tsadarsh0707@gmail.com>
2024-09-04 10:01:03 -0400

    draws major axis on the the orbit plot

--- satcom.py
+++ satcom.py
@@ -8,7 +8,7 @@ Kp = G * Mp
 r_n = 20_000 # km
 theta_n = 0 # rad
 
-vr_n = 0 # km/s
+vr_n = -3 # km/s
 vt_n = 5 # km/s
 
 del_t = 10 # secs
@@ -17,36 +17,33 @@ del_r_n = vr_n * del_t
 del_theta_n = vt_n * del_t / r_n
 
 r, theta = [r_n], [theta_n]
-
-Tp = 0
-theta_cache = 1.57
+min_r, max_r = 10**10, 0
+th_min, th_max = 0, 0
 
 for i in range(1, 50000):
-    Tp += del_t
-
     r_n1 = r_n + del_r_n
     theta_n1 = (theta_n + del_theta_n)
     del_r_n1 = del_r_n + (((r_n + (0.5 * del_r_n)) * (del_theta_n ** 2)) - (Kp * (del_t ** 2) / (r_n ** 2)))
     del_theta_n1 = (del_theta_n - ((2 * del_r_n * del_theta_n) / (r_n + 0.5 * del_r_n)))
-    
-    # if theta_cache > (theta_n1 % 3.14):
-    #     print("Time period:", Tp, "seconds")
-    #     Tp = 0
-    # else:
-    #     theta_cache = theta_n1 % 3.14
-    
-    #print(r_n1, theta_n1, del_r_n1, del_theta_n1)
-    
+
     r.append(r_n1)
     theta.append(theta_n1)
-
+    if min_r > r_n1:
+        min_r = r_n1
+        th_min = theta_n1
+    elif max_r < r_n1:
+        max_r = r_n1
+        th_max = theta_n1
+    
     r_n = r_n1
     theta_n = theta_n1
     del_r_n = del_r_n1
     del_theta_n = del_theta_n1
 
-#fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-#ax.plot(theta, r)
+
+fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+ax.plot(theta, r)
+ax.plot([th_min, th_max], [min_r, max_r])
 bro = [i % 6.28 for i in theta]
 count = 0
 for i in range(1, len(bro)):
@@ -61,5 +58,5 @@ for i in range(1, len(bro)):
 #         f.write(str(i))
 #         f.write("\n")
 # # pprint(str(bro))
-plt.plot(bro)
+# plt.plot(bro)
 plt.show()
