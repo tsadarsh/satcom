667f677f36e292f251c44aacf5193975ed157f80
Adarsh <tsadarsh0707@gmail.com>
2024-09-04 10:39:36 -0400

    Orbit calculation params for Haley's comet

--- satcom.py
+++ satcom.py
@@ -1,17 +1,20 @@
 import matplotlib.pyplot as plt
 from pprint import pprint
 
-G = 6.674 * (10 ** (-20)) # km^3 kg^-1 s^-2
-Mp = 5.972 * (10 ** 24) # kg
+# G = 6.674 * (10 ** (-20)) # km^3 kg^-1 s^-2
+G = 6.674 * (10 ** (-29)) # Mkm^3 kg^-1 s^-2
+#Mp = 5.972 * (10 ** 24) # kg
+Mp = 1.9891 * (10 ** 30) # kg (sun)
+
 Kp = G * Mp
 
-r_n = 20_000 # km
+r_n = 5_270_000 # Mkm
 theta_n = 0 # rad
 
 vr_n = 0 # km/s
-vt_n = 5 # km/s
+vt_n = 0.00091 # km/s
 
-del_t = 10 # secs
+del_t = 100 # secs
 
 del_r_n = vr_n * del_t
 del_theta_n = vt_n * del_t / r_n
@@ -20,7 +23,7 @@ r, theta = [r_n], [theta_n]
 min_r, max_r = 10**10, 0
 th_min, th_max = 0, 0
 
-for i in range(1, 50000):
+for i in range(1, 100000000):
     r_n1 = r_n + del_r_n
     theta_n1 = (theta_n + del_theta_n)
     del_r_n1 = del_r_n + (((r_n + (0.5 * del_r_n)) * (del_theta_n ** 2)) - (Kp * (del_t ** 2) / (r_n ** 2)))
@@ -53,7 +56,7 @@ bro = [i % 6.28 for i in theta]
 count = 0
 for i in range(1, len(bro)):
     if bro[i-1] > bro[i]:
-        print(count * del_t / (60 * 24))
+        print(count * del_t / (60 * 24)) #hours
         count = 0
     else:
         count += 1
